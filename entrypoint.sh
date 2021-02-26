f53b728054aad425d6de641404b112fcf439d3bd
Robin Godwyll <rodwyll+github@gmail.com>
2021-02-26 14:04:10 +0100

    Node_options --max_old_space_size

--- entrypoint.sh
+++ entrypoint.sh
@@ -17,6 +17,7 @@ npm install hexo-cli -g
 npm install hexo-deployer-git --save
 npm install -g increase-memory-limit
 increase-memory-limit
+export NODE_OPTIONS="--max_old_space_size=8192"
 
 # deployment
 if [ "$INPUT_COMMIT_MSG" = "none" ]
