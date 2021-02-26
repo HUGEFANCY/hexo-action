a3ecf932eaf6abb863401fd2005aa0bd2d0814ad
Robin Godwyll <rodwyll+github@gmail.com>
2021-02-26 13:54:47 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -15,7 +15,7 @@ git config --global user.email "$INPUT_USER_EMAIL"
 # install hexo env
 npm install hexo-cli -g
 npm install hexo-deployer-git --save
-npm npm install -g increase-memory-limit
+npm install -g increase-memory-limit
 increase-memory-limit
 
 # deployment
